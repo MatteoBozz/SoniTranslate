9d3136a08bc469cb5e0608e47249ba96f737eb57
MatteoBozz <bozz87@gmail.com>
2026-08-28 00:45:35 +0200

    get clean translation from json response

--- soni_translate/translate_segments.py
+++ soni_translate/translate_segments.py
@@ -213,7 +213,7 @@ def call_gpt_translate(
         ]
     )
     result = response.choices[0].message.content
-    logger.info(f"Response: {str(Response)}")
+    logger.info(f"Response: {str(Response)} ---END")
     logger.debug(f"Result: {str(result)}")
 
     try:
